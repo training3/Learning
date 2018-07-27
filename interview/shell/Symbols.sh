2e0582e0b0331d0c5a99ae899d5e21f62e7563f9
training3 <36807842+training3@users.noreply.github.com>
2018-07-27 19:46:44 +0530

    FNR in awk

--- interview/shell/Symbols.sh
+++ interview/shell/Symbols.sh
@@ -1,2 +1,14 @@
 #1 - \t - Create a tab
 echo "Hi\tHello"
+
+#2 - FNR in awk - prints only specific line in a column
+# example 
+cat text.txt
+name address pincode
+buvan tirupur 641605
+ganesh chennai 600601 
+ramesh ramnagar 600602
+
+cat text.txt |awk 'FNR 3 {print $2}'
+chennai 
+# it prints 3rd line 2nd column
