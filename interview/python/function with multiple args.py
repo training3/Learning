6ba220779cfe06758ec01467ed0b235189db00b9
training3 <36807842+training3@users.noreply.github.com>
2018-07-31 09:36:39 +0530

    Function with multiple args

---
+++ interview/python/function with multiple args.py
@@ -0,0 +1,6 @@
+def func_arg(name, age, number):
+    print("Hi",name + "!" + " Your age is:",age + " and your number is: " + number)
+
+func_arg("Ganesh", "24", "12")
+func_arg("Ramesh", "23", "15")
+func_arg("Sathish", "23", "18")
